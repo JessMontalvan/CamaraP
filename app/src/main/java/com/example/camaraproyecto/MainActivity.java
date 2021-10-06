4f7dd3aa14159e6386e896a2fd2aae42e459daa1
Jessica <jnmontalvan@tecnologicoloja.edu.ec>
2021-10-05 23:45:18 -0500

    Ventana

---
+++ app/src/main/java/com/example/camaraproyecto/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.camaraproyecto;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.app.ListActivity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.icon_eliminar_todo: {
+                AlertDialog.Builder dialogo1 = new AlertDialog.Builder(this);
+                dialogo1.setTitle("Eliminar imagen");
+                dialogo1.setMessage("¿Deseas eliminar esta imagen?");
+                dialogo1.setCancelable(false);
+                dialogo1.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialogo1, int id) {
+                        Toast t = Toast.makeText(MainActivity.this, "Imagen eliminada", Toast.LENGTH_SHORT);
+                        t.show();
+
+                    }
+                });
+                dialogo1.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialogo1, int id) {
+
+                    }
+                });
+                dialogo1.show();
+                break;
+            }
+            default:
+                break;
+        }
+        return true;
+    }
+
+}
